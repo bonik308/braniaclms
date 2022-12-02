4bcdb49a5059521ee8064bd01787ac4ff51b3e43
bonik308 <lol>
2022-12-02 23:29:06 +0300

    lesson_2 Static site with Django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,21 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
 
-def hello_world(request):
-    return HttpResponse("Hello world")
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
 
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
 
-def check_kwargs(request, **kwargs):
-    return HttpResponse(f"kwargs:<br>{kwargs}"
-    )
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
 
 # Create your views here.
